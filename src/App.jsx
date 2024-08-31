bef11c5ff0cbef48edae35b0ac3c2e13d29bbb04
asilbekcodes <asilbeksuvonov43@gmail.com>
2024-08-31 13:25:58 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route,  Routes, } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route,  Routes, useNavigate, } from 'react-router-dom'
 import Main from './pages/Main'
 import Register from './pages/auth/Register'
 import Login from './pages/auth/Login'
@@ -9,11 +9,10 @@ import Seller from './pages/seller/Seller'
 import Product from './pages/admin/Product'
 import Category from './pages/admin/Category'
 import Users from './pages/admin/Users'
+import { config } from './helpers/token'
 function App() {
   // let navigater = useNavigate()
-  // useEffect(() => {
-  //   checkIsLogin()
-  // },[])
+  
 
   // const checkIsLogin = () => {
   //   // const token = localStorage.getItem('token')
@@ -22,9 +21,13 @@ function App() {
   //   if (!token) {
   //     navigater('/login')
   //   }else{
-  //     navigater('/')
+  //     navigater('/register')
   //   }
   // }
+  
+  // useEffect(() => {
+  //   checkIsLogin()
+  // },[])
 
   // const navigate = useNavigate()
   // if (Login === false) {
@@ -33,15 +36,15 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Main />} />
         <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path='/buyer' element={<Buyer/>}/>
-        <Route path='/admin' element={<Admin/>}/>
         <Route path='/seller' element={<Seller/>}/>
-        <Route path="/product" element={<Product />} />
-        <Route path='/category' element={<Category/>}/>
-        <Route path="/users" element={<Users />} />
+        {/* --- admin pages --- */}
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path="/admin/product" element={<Product />} />
+        <Route path='/admin/category' element={<Category/>}/>
+        <Route path="/admin/users" element={<Users />} />
       </Routes>
     </div>
   )
