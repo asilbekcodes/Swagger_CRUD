8135b41bc091f433a60e3afc5c5c9f312c4b839b
Asilbek <asilbek45@example.com>
2024-08-31 13:12:56 +0500

    admin header add

---
+++ src/components/adminHeader.jsx
@@ -0,0 +1,62 @@
+import React, {  useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function AdminHeader() {
+    const [isOpenDrop, setIsOpenDrop] = useState(false)
+    const openDrop = () => {
+        setIsOpenDrop(!isOpenDrop)
+    }
+    const navigater = useNavigate()
+
+
+    const signIn = () => {
+        localStorage.removeItem('token')
+        navigater("/")
+        openDrop()
+    }
+    const signUp = () => {
+      localStorage.removeItem('token')
+      navigater("/register")
+      openDrop()
+    }
+    return (
+        <div className='bg-white h-16'>
+        <nav class="fixed w-full z-20 top-0 start-0 border-b border-gray-200 bg-white">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap ">Flowbite</span>
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <p onClick={signIn} className='cursor-pointer mx-5 font-semibold'>Sign in </p>          
+                <p onClick={signUp} className='cursor-pointer mx-5 font-semibold'>Sign up </p>
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+                    <li>
+                        <Link to={'/admin'} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  " aria-current="false">Home</Link>
+                    </li>
+                    <li>
+                        <Link to={'/admin/users'} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "aria-current="/admin/users">Users</Link>
+                    </li>
+                    <li>
+                        <Link to={'/admin/category'} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Categorys</Link>
+                    </li>
+                    <li>
+                        <Link to={'/admin/product'} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Products</Link>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        </div>
+    )
+}
+
+export default AdminHeader
