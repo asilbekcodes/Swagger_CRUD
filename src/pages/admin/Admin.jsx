0e96b6137907e0f24cef0736aaa2631ffc87a601
Asilbek <asilbek45@example.com>
2024-08-31 13:05:55 +0500

    admin pages add

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,32 +1,24 @@
 import React from 'react'
+import AdminHeader from '../../components/adminHeader'
 import { Link } from 'react-router-dom'
-
+import Footer from '../../components/Footer'
 function Admin() {
   return (
-    <div className='container m-auto my-5'>
-      <h1 className='text-3xl text-red-600 mb-5 font-serif'>Hello Admin</h1>
-      <Link to={'/users'}>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white  focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
-          user-controller
-          </span>
-        </button>
-      </Link>
-      <Link to={'/category'}>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
-          category-controller
-          </span>
-        </button>
-      </Link>
-      <Link to={'/product'}>
-        <button class=" relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
-          product-controller
-          </span>
-        </button>
-      </Link>
-    </div>
+    <>
+      <AdminHeader />
+        <div className='container m-auto my-5 flex justify-between gap-10 h-[54vh]'>
+          <Link to={"/admin/users"} className='w-full'>
+            <button type="button" className="w-full h-32 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2">Users</button>
+          </Link >
+          <Link to={"/admin/category"} className='w-full'>
+            <button type="button" className="w-full h-32 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2">Categorys</button>
+          </Link>
+          <Link to={"/admin/product"} className='w-full'>
+            <button type="button" className="w-full h-32 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2">Products</button>
+          </Link>
+      </div>
+      <Footer />
+    </>
   )
 }
 
