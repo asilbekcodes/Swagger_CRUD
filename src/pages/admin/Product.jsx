e12ab619479b6127373956e0504589cfab12a77f
Asilbek <asilbek45@example.com>
2024-08-31 13:07:09 +0500

    product pages add

--- src/pages/admin/Product.jsx
+++ src/pages/admin/Product.jsx
@@ -3,11 +3,16 @@ import { url } from '../../helpers/url'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { config } from '../../helpers/token'
+import AdminHeader from '../../components/adminHeader'
+import Footer from '../../components/Footer'
 
 
 function Product() {
     const [products, setProducts] = useState(null)
 
+    const [selectID, setSelectID] = useState(null)
+    const [deleteModal, setDeleteModal] = useState(false)
+    const openDelete = () => setDeleteModal(!deleteModal)
 
     function product() {
         axios.get(`${url}product/list`)
@@ -27,14 +32,15 @@ function Product() {
         product()
     }, [])
 
-    function deleteProduct(productId) {
-        if (config && productId) {
-            axios.delete(`${url}product/${productId}`, config)
+    function deleteProduct() {
+        if (config && selectID) {
+            axios.delete(`${url}product/${selectID}`, config)
                 .then(response => {
                     toast.success('Product deleted successfully')
                     if (response.data.success) {
                         toast.success('Product deleted successfully')
                         product()
+                        setDeleteModal(false)
                     } else {
                         toast.error('Failed to delete product')
                     }
@@ -46,7 +52,10 @@ function Product() {
         }
     }
   return (
-    <div className='container mx-auto my-5'>
+    <>
+    <AdminHeader />
+    <div className='container mx-auto my-5 h-[54vh]'>
+      <h4 className='text-3xl font-serif font-bold text-green-500 mb-5'>Products</h4>
         <div className="relative overflow-x-auto border border-black border-solid rounded-xl">
           <table className="w-full text-sm text-left rtl:text-right  ">
               <thead className="text-xs text-gray-900 uppercase ">
@@ -84,7 +93,7 @@ function Product() {
                           <td className="px-6 py-4 text-gray-900">
                             {item.price} so'm
                           </td>
-                          <button onClick={() => deleteProduct(item.id)} type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                          <button onClick={() => {openDelete(); setSelectID(item.id)}} type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                       </tr>
                   ) : <tr ><td className='px-6 py-4'>data is empty...</td></tr>
                 }
@@ -93,6 +102,33 @@ function Product() {
           </table>
       </div>
     </div>
+    {
+        deleteModal && 
+        <div id="popup-modal" tabindex="-1" class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div class="relative p-4 w-full max-w-md max-h-full">
+              <div class="relative bg-white rounded-lg shadow drop-shadow-2xl ">
+                  <button onClick={openDelete} type="button" class="absolute top-3 end-2.5 text-gray-700 bg-transparent hover:bg-gray-200 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center  " data-modal-hide="popup-modal">
+                      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                      </svg>
+                      <span class="sr-only">Close modal</span>
+                  </button>
+                  <div class="p-4 md:p-5 text-center">
+                      <svg class="mx-auto mb-4 text-gray-700 w-12 h-12 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                      </svg>
+                      <h3 class="mb-5 text-lg font-normal text-gray-700 ">Are you sure you want to delete this product?</h3>
+                      <button onClick={deleteProduct} data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                          Yes, I'm sure
+                      </button>
+                      <button onClick={openDelete} data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+       }
+       <Footer/>
+    </>
   )
 }
 
